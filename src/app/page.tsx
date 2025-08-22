1461a7df3ec19037d85a02c3dc204a169f34d111
blackboxai-deploy <deploy@blackboxai.dev>
2025-08-22 06:47:52 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-white flex items-center justify-center">
+      <div className="w-4 h-4 bg-black rounded-full"></div>
+    </div>
+  );
+}
